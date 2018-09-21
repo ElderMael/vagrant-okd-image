37e43f1b04c28ee7fae17dbe3f6ea86ff2a59ca4
ElderMael <sftwr.mael@gmail.com>
2018-09-21 16:56:54 -0500

    Add Service Account And Authentication

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,7 @@ set -ex
 
 yum install -y epel-release > /dev/null 2>&1
 yum update -y > /dev/null 2>&1
-yum install -y python-pip python2-httpie.noarch wget docker git emacs > /dev/null 2>&1
+yum install -y python-pip python2-httpie.noarch wget docker git emacs httpd-tools > /dev/null 2>&1
 
 # We require to setup an insecure registry for some
 # images installed by openshift
@@ -36,7 +36,23 @@ pushd "${HOME}"
     sed --in-place='' "s/\${githubOrganization}/${GITHUB_ORGANIZATION}/g" /tmp/master-config.yaml
 
     mv -vf /tmp/master-config.yaml /var/lib/origin/openshift.local.config/master/
+    htpasswd -b -c /var/lib/origin/openshift.local.config/master/htpasswords \
+        serviceacc "${OPENSHIFT_SERVICE_ACCOUNT_PASSWORD}"
 
     oc cluster up --use-existing-config=true
+
+    while ! http --check-status --verify=no GET https://localhost:8443/
+    do
+        echo '.'; sleep 5s
+    done
+
+    sleep 5s
+
+    oc login -u serviceacc -p "${OPENSHIFT_SERVICE_ACCOUNT_PASSWORD}"
+
+    oc new-project pet-projects --description="Pet Projects" --display-name="Projects"
+
+    oc project pet-projects
+
 
 popd
