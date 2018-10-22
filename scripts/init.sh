fc5839d1cfaf211f0864573e4494cc4ab31e6e03
ElderMael <sftwr.mael@gmail.com>
2018-10-22 03:27:17 -0500

    Add SCC Privileges To More Users

--- scripts/init.sh
+++ scripts/init.sh
@@ -35,8 +35,8 @@ pushd "${HOME}"
 
     oc login -u system:admin
 
-    oc adm policy add-scc-to-user anyuid useroot
     ac adm policy add-scc-to-user anyuid default
+    oc adm policy add-scc-to-user anyuid userroot
 
     oc adm policy add-cluster-role-to-group admin system:authenticated
 
