e23c47c1579abdaff86820c5c4ae12ccfc8cd3d9
ElderMael <sftwr.mael@gmail.com>
2018-10-22 04:12:33 -0500

    Add Permissions For Admin

--- scripts/init.sh
+++ scripts/init.sh
@@ -35,16 +35,20 @@ pushd "${HOME}"
 
     oc login -u system:admin
 
-    oc adm policy add-scc-to-user privileged default
-    oc adm policy add-scc-to-user privileged userroot
 
     # Pet Projects Namespace
     oc new-project pet-projects --description="Pet Projects" --display-name="Projects"
     oc new-app 'https://github.com/ElderMael/discord-ts'
+    oc adm policy add-scc-to-user privileged default
+    oc adm policy add-scc-to-user privileged userroot
+    oc adm policy add-role-to-user admin "system:admin"
 
     # Tooling Namespace
     oc new-project tools --description="Tools that run on the server" --display-name="Tools"
     oc process -f /tmp/oc_templates/vpn_secret.yaml | oc apply -f -
+    oc adm policy add-scc-to-user privileged default
+    oc adm policy add-scc-to-user privileged userroot
+    oc adm policy add-role-to-user admin "system:admin"
 
     oc cluster down
 
@@ -67,7 +71,7 @@ pushd "${HOME}"
     sleep 30s
 
     oc login -u system:admin
-    oc adm policy add-role-to-user admin "${GITHUB_USERNAME}"
-    oc adm policy add-role-to-user admin "${GITHUB_USERNAME}"
+    oc adm policy add-role-to-user -n tools admin "${GITHUB_USERNAME}"
+    oc adm policy add-role-to-user -n pet-projects admin "${GITHUB_USERNAME}"
     
 popd
