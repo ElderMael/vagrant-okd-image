01c830926c52943539101b6628c5192b320294ac
ElderMael <sftwr.mael@gmail.com>
2018-10-22 03:43:30 -0500

    Add Privileged Permission To Users For VPN

--- scripts/init.sh
+++ scripts/init.sh
@@ -35,8 +35,8 @@ pushd "${HOME}"
 
     oc login -u system:admin
 
-    oc adm policy add-scc-to-user anyuid default
-    oc adm policy add-scc-to-user anyuid userroot
+    oc adm policy add-scc-to-user privileged default
+    oc adm policy add-scc-to-user privileged userroot
 
     oc adm policy add-cluster-role-to-group admin system:authenticated
 
